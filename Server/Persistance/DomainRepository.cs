245527c0f7015a873dde6f0e1025acdf98b8ee73
Oliwier Nowak <NOW20328@spengergasse.at>
2023-10-11 01:27:03 +0200

    progress

---
+++ Server/Persistance/DomainRepository.cs
@@ -0,0 +1,32 @@
+﻿using Agendo.Server.Models;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.Common;
+
+namespace Agendo.Server.Persistance
+{
+    public interface IDomainRepository
+    {
+        List<DomainDTO> GetAll(); 
+    }
+
+    public class DomainRepository : IDomainRepository
+    {
+        private readonly IConfiguration _config;
+
+        private  IDbConnection _connection;
+
+        public DomainRepository(IConfiguration config)
+        {
+            _config = config;
+
+           // _connection = new DbConnection(_configuration.GetSection("AppSettings:Token").Value!);
+        }
+
+        public List<DomainDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
