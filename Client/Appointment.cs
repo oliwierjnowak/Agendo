e05cbc2ffc0fe488efcfdca0db436072de08b01a
Philipp Schaffer <philippschaffer@Philipps-MacBook-Pro.local>
2023-10-11 11:56:16 +0200

    added schaffers yearly scheduler

---
+++ Client/Appointment.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Agendo.Client
+{
+	public class Appointment
+	{
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+        public string Text { get; set; }
+}
+}
+
--- Client/Program.cs
+++ Client/Program.cs
@@ -3,10 +3,12 @@ global using Blazored.LocalStorage;
 using Agendo.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddScoped<DialogService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
