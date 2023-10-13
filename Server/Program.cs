0ad551ccd8bd99c85615ec99faa4c3c0da047370
Philipp Schaffer <philippschaffer@Philipps-MacBook-Pro.local>
2023-10-13 11:53:56 +0200

    added create-edit shift basic structure

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.ResponseCompression;
+using Radzen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddRadzenComponents();
 
 var app = builder.Build();
 
