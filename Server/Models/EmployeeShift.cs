85e32b7a3c954cc4ab6d882e3b503781d8505e5e
Anton Schubhart <SCH20428@spengergasse.at>
2023-10-14 19:54:56 +0200

    Added Layered Architecture

---
+++ Server/Models/EmployeeShift.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agendo.Server.Models
+{
+    public record EmployeeShift
+    {
+        public int EmpNr { get; set; }
+        public int ISOWeek { get; set; }
+        public int ISOYear { get; set; }
+        public int DayOfWeek { get; set; }    
+        public int ShiftNR { get; set; }
+        public string ShiftName { get; set; }
+        public int ShiftHours { get; set; }
+
+    }
+}
