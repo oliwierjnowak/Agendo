b274274aed5602e02ad2c02c9406523b22ada3e7
Oliwier Nowak <NOW20328@spengergasse.at>
2023-11-23 20:16:15 +0100

    private httpclient

---
+++ Client/HttpClients/ScheduleManagementClient.cs
@@ -0,0 +1,28 @@
+﻿using Agendo.Server.Models;
+using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
+
+namespace Agendo.Client.HttpClients
+{
+    public interface IScheduleManagementClient
+    {
+       Task<IEnumerable<EmployeeShiftDTO>> GetEmployeeShift(int EmpNr);
+    }
+
+    public class ScheduleManagementClient : IScheduleManagementClient
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public ScheduleManagementClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<EmployeeShiftDTO>> GetEmployeeShift(int EmpNr)
+        {
+            var x = await _httpClient.GetFromJsonAsync<IEnumerable<EmployeeShiftDTO>>("api/EmployeeShift?Emp=" + EmpNr);
+            return x;
+        }
+    }
+}
