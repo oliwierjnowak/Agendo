8c9d99e5f16bcaa2db8ac5c8bfa8610423b7683a
Oliwier Nowak <NOW20328@spengergasse.at>
2023-12-09 21:59:08 +0100

    new endpoint for getting the emp that work during specific shift

--- Server/Services/DomainService.cs
+++ Server/Services/DomainService.cs
@@ -1,5 +1,6 @@
 ﻿using Agendo.Server.Models;
 using Agendo.Server.Persistance;
+using System.Globalization;
 
 namespace Agendo.Server.Services
 {
@@ -7,6 +8,7 @@ namespace Agendo.Server.Services
     {
         Task<List<DomainDTO>> GetAllAsync(int superior);
         Task<List<DomainDTO>> GetListAsync(int superior, IEnumerable<int> domains);
+        Task<IEnumerable<DomainDTO>> GetShiftEmployees(int user, DateTime start, int shiftNR);
     }
 
     public class DomainService(IDomainRepository _domainRepository) : IDomainService
@@ -19,6 +21,20 @@ namespace Agendo.Server.Services
         public async Task<List<DomainDTO>> GetListAsync(int superior, IEnumerable<int> domains)
         {
             return await _domainRepository.GetListAsync(superior,domains);
+        }
+
+        public async Task<IEnumerable<DomainDTO>> GetShiftEmployees(int user, DateTime start, int shiftNR)
+        {
+            var year = start.Year;
+
+            // Create an instance of the GregorianCalendar class with the ISO 8601 rule
+            Calendar calendar = new GregorianCalendar(GregorianCalendarTypes.USEnglish);
+
+            // Get ISO week number
+            int isoWeekNumber = calendar.GetWeekOfYear(start, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            DayOfWeek dayOfWeek = start.DayOfWeek;
+            var employees = await _domainRepository.GetShiftEmployees(user, year, isoWeekNumber, dayOfWeek, shiftNR);
+            return employees;
         }
     }
 }
