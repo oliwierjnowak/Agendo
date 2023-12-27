0eb69871fae5b8c99d341739101f1e1b68d04a83
onow <oliwierjnowak@gmail.com>
2023-12-27 16:09:38 +0100

    refactoring httpClient finished

--- Client/Program.cs
+++ Client/Program.cs
@@ -10,7 +10,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IScheduleManagementClient, ScheduleManagementClient>();
+builder.Services.AddScoped<IApiClient, ApiClientProvider>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 builder.Services.AddAuthorizationCore();
 builder.Services.AddRadzenComponents();
